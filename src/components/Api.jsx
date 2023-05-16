e701911d5e6d0de8337e3960a829645825d4cce2
Nour-Agha99 <temoagha@gmail.com>
2023-05-16 22:13:44 +0300

    create main API

---
+++ src/components/Api.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Api = () => {
+  return (
+    <>
+      <div className="😐">
+        <div className="heading">
+          <h2>Trending</h2>
+          <div className="today-week">
+            <div className="today">Today</div>
+            <div className="Weekly">This Week</div>
+          </div>
+        </div>
+        <div className="😎">
+          <div className="percentage"></div>
+          <div className="poster">
+            <img alt="" src="" />
+          </div>
+          <div className="content"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Api;
